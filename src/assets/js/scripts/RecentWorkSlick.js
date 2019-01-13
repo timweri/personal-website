e012ba2b9803db4e5fe57c10b3df997d3e1cb707
Tim Weri <ng.duc.tahn@gmail.com>
2019-01-12 22:57:14 -0500

    Edit slick arrows

--- src/assets/js/scripts/RecentWorkSlick.js
+++ src/assets/js/scripts/RecentWorkSlick.js
@@ -1,10 +1,23 @@
 (function() {
     $('.recent-work-group').slick({
         autoplay: true,
+        autoplaySpeed: 5000,
         mobileFirst: true,
         centerMode: true,
-        dots: true,
-        adaptiveHeight: true
+        centerPadding: '20px',
+        arrows: false,
+        dots: false,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    arrows: true,
+                    centerPadding: '40px'
+                }
+            }
+        ]
     })
-    console.log('loaded slick')
 })();
